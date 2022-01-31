19c1c438272770975df9ebcb66900c9c682461c9
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-01-31 18:53:58 +0000

    header to use useState

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="Header">
+      <h1>Weather App</h1>
+      {/* <button onClick={() => setDarkMode(!darkMode)} type="button">
+        {darkMode ? "Dark Mode" : "Light Mode"}
+      </button> */}
+    </div>
+  );
+};
